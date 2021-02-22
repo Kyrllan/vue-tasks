048c4a841d063c12ba13f5e1d56419c5fff0c41f
Kyrllan Nogueira <kyrllan@webcgi.com.br>
2021-02-22 10:44:38 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tarefas</h1>
+    <TaskProgress :progress="progress" />
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid
+      :tasks="tasks"
+      @taskStateChange="toggleTaskState"
+      @taskDeleted="deleteTask"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskGrid from './components/TaskGrid'
+import NewTask from './components/NewTask'
+import TaskProgress from './components/TaskProgress'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    TaskGrid, NewTask, TaskProgress
+  },
+
+  data: () => ({
+    tasks: []
+  }),
+
+  created () {
+    const json = localStorage.getItem('tasks')
+    this.tasks = JSON.parse(json) || []
+  },
+
+  computed: {
+    progress () {
+      const total = this.tasks.length
+      const done = this.tasks.filter(t => !t.pending).length
+      return Math.round(done / total * 100) || 0
+    }
+  },
+
+  watch: {
+    tasks: {
+      deep: true,
+      handler () {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      }
+    }
+  },
+
+  methods: {
+    addTask (task) {
+      const sameName = t => t.name === task.name
+      const reallyNew = this.tasks.filter(sameName).length === 0
+      if (reallyNew) {
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true
+        })
+      }
+    },
+    deleteTask (i) {
+      this.tasks.splice(i, 1)
+    },
+    toggleTaskState (i) {
+      this.tasks[i].pending = !this.tasks[i].pending
+    }
   }
 }
 </script>
 
-<style lang="scss">
+<style>
+@import url("https://fonts.googleapis.com/css?family=Yellowtail");
+
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to top, rgb(173, 83, 137), rgb(60, 16, 83));
+  color: #fff;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-family: "Yellowtail";
+  font-size: 150px;
+  font-weight: 300;
+  color: white;
 }
 </style>
